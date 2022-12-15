f65e295622475e7b0df96237e8b6c5decd32ee1b
Euihyun Lee <lee@euihyun.dev>
2022-12-15 15:54:57 +0900

    feat(ch1): exercise 1-10

---
+++ ch1_A_Tutorial_Introduction/ex_1_10.c
@@ -0,0 +1,30 @@
+/*
+ * Exercise 1-10. Write a program to copy its input to its output, replacing each
+ * tab by \t, each backspace by \b, and each backslash by \\. This makes tabs
+ * and backspaces visible in an unambiguous way.
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		if (c == '\t') {
+			putchar('\\');
+			putchar('t');
+			continue;
+		}
+		if (c == '\b') {
+			putchar('\\');
+			putchar('b');
+			continue;
+		}
+		if (c == '\\') {
+			putchar('\\');
+			putchar('\\');
+			continue;
+		}
+		putchar(c);
+	}
+}
